c99579bc1902b29b86817a6bce81d5e35e33c183
Vegard Harper <vegahar@stud.ntnu.no>
2014-01-26 15:12:17 +0100

    added conf/evolutions

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,27 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table local_group (
+  id                        varchar(255) not null,
+  name                      varchar(255) not null,
+  constraint uq_local_group_name unique (name),
+  constraint pk_local_group primary key (id))
+;
+
+create sequence local_group_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists local_group;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists local_group_seq;
+
